4b691486091e581b70379e231804d95a6b839134
lipholo-dev <lipholo.info@gmail.com>
2025-04-09 09:27:21 +0200

    initial commit

---
+++ mySite/mySite/personal/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('about/', views.about, name='about'),
+    path('index/', views.index, name='index'),
+    path('store/', views.store, name='store'),
+]
